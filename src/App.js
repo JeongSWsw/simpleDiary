78d180be02a480331c2cd70ae0e291c4a465e20d
JeongSWsw <jeongsw34@gmail.com>
2023-06-08 16:50:19 +0900

    create diary item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: "정승원",
+//     content: "hi1",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: "정",
+//     content: "hi2",
+//     emotion: 3,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: "승",
+//     content: "hi3",
+//     emotion: 2,
+//     created_date: new Date().getTime(),
+//   },
+// ];
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]); //새로운 일기가 제일 위로 오게끔
+  };
+
   return (
     <div className="App">
-      <DiaryEditor />
+      <DiaryEditor onCreate={onCreate} /> {/* prop으로 전달 */}
+      <DiaryList diaryList={data} /> {/* prop으로 전달 */}
     </div>
   );
 }
