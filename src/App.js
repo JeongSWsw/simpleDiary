0f31a565a0d2b42235049e2f03e38fa63a9ed505
JeongSWsw <jeongsw34@gmail.com>
2023-06-09 17:07:33 +0900

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import OptimizeTest from "./OptimizeTest";
 
 function App() {
   const [data, setData] = useState([]);
@@ -30,7 +29,7 @@ function App() {
     getData();
   }, []);
 
-  const onCreate = (author, content, emotion) => {
+  const onCreate = useCallback((author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {
       author,
@@ -40,8 +39,8 @@ function App() {
       id: dataId.current,
     };
     dataId.current += 1;
-    setData([newItem, ...data]); //새로운 일기가 제일 위로 오게끔
-  };
+    setData((data) => [newItem, ...data]); //새로운 일기가 제일 위로 오게끔
+  }, []);
 
   const onRemove = (targetId) => {
     const newDiaryList = data.filter((it) => it.id !== targetId);
@@ -68,7 +67,6 @@ function App() {
 
   return (
     <div className="App">
-      <OptimizeTest />
       <DiaryEditor onCreate={onCreate} /> {/* prop으로 전달 */}
       <div>전체 일기 : {data.length}</div>
       <div>기분 좋은 일기 개수: {goodCount}</div>
